542a066385d7580d518f97ee643ccf2869e53d61
Selco Drapalla <gargamelsave90@gmail.com>
2023-06-07 12:38:23 +0200

    add fetch-the-data function

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+const url = "https://rickandmortyapi.com/api/character";
 
 // States
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } else {
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    console.error("An Error occured");
+  }
+}
+fetchCharacters();
