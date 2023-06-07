390a20352cb9a18079421e57e60ad5d65ac5f2bb
Mona Herter <hertermona@gmail.com>
2023-06-07 11:46:48 +0200

    create card-function

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,27 @@
-export function createCharacterCard() {}
+export function createCharacterCard(character) {
+  const newCard = document.createElement("li");
+  newCard.classList.add("card");
+
+  newCard.innerHTML = `
+      <div class="card__image-container">
+        <img
+          class="card__image"
+          src="${character.results.image}"
+          alt="${character.results.name}"
+        />
+        <div class="card__image-gradient"></div>
+      </div>
+      <div class="card__content">
+        <h2 class="card__title">${character.results.name}</h2>
+        <dl class="card__info">
+          <dt class="card__info-title">Status</dt>
+          <dd class="card__info-description">${character.results.status}</dd>
+          <dt class="card__info-title">Type</dt>
+          <dd class="card__info-description">${character.results.type}</dd>
+          <dt class="card__info-title">Occurrences</dt>
+          <dd class="card__info-description">${character.results.occurrences}</dd>
+        </dl>
+      </div>`;
+
+  return newCard;
+}
